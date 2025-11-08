b8a31e95560925a866076bf41aa3934f9eb89931
ZakiStCloud <mf3237bb@go.minnstate.edu>
2025-11-07 21:26:07 -0600

    Set up WarehouseContext with state management
    Initialize WarehouseContext with states and nextState matrix.

--- WarehouseContext.java
+++ WarehouseContext.java
@@ -7,6 +7,31 @@ import java.io.*;
 
 
 public class WarehouseContext{
+  private static int currentState;
+  private static Warehouse warehouse;
+  private static WarehouseContext context;
+  public static final int IsUser = 1;
+  public static final int IsClerk = 2;
+  public static final int IsManager = 3;
+  private static WarehouseState[] states;
+  private int[][] nextState;
+
+
+private WarehouseContext{
+
+  states = new WarehouseState[4];
+  states[3] = ManagerState.instance();
+  states[2] = Clerkstate.instance();
+  states[1] = Userstate.instance(); 
+  states[0] = Loginstate.instance();
+
+  currentState = 0;
+  nextState = new int[4][4];
+  nextState[0][0] = -1;nextState[0][1] = 1;nextState[0][2] = 2;nextState[0][2] = 3;
+  nextState[1][0] = 0;nextState[1][1] = 1;nextState[1][2] = -2;nextState[1][3] = -2;
+  nextState[2][0] = 0;nextState[2][1] = 1;nextState[2][2] = 2;nextState[2][3] = -2;
+  nextState[3][0] = 0;nextState[3][1] = 1;nextState[3][2] = 2;nextState[3][3] = 3;
+}
   
 //Prompts the user and retrieves their input
 public String getToken(String prompt){ 
