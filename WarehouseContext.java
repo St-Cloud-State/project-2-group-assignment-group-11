29db453a62c47df75508aea2a9fe16c96d73b456
ZakiStCloud <mf3237bb@go.minnstate.edu>
2025-11-07 20:16:15 -0600

    Add files via upload

---
+++ WarehouseContext.java
@@ -0,0 +1,48 @@
+public class WarehouseContext{
+  
+//Prompts the user and retrieves their input
+public String getToken(String prompt){ 
+  
+} 
+  
+//Prompts the user with yes or no and returns bool
+private boolean yesOrNo(String prompt){
+  
+}
+
+//Stores user type (clerk/manager/client)
+public void setLogin(int code){
+  
+} 
+//Returns user type
+public int getLogin(){
+  
+} 
+ //Stores user login ID
+public void setUser(String uID){
+  
+}
+public String getUser(){
+  
+}
+//Initiates the change of state based on current and ddesired state
+public void changeState(int transition){
+  
+} 
+
+//Exits the program
+private void terminate(){
+  
+} 
+
+//Runs the state
+public void process(){
+  
+}
+
+//Creates the singleton instance of the class
+public static WarehouseContext instance(){
+  
+}
+
+}
