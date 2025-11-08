704f98184dfa6b069a5b54dd47031a31ae93c8c7
ZakiStCloud <mf3237bb@go.minnstate.edu>
2025-11-07 20:38:36 -0600

    implement getToken and yesOrNo
    Refactor getToken method to use StringTokenizer for input parsing.

--- WarehouseContext.java
+++ WarehouseContext.java
@@ -1,13 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.text.*;
+import java.io.*;
+
+
 public class WarehouseContext{
   
 //Prompts the user and retrieves their input
 public String getToken(String prompt){ 
-  
+  do {
+      try {
+        System.out.println(prompt);
+        String line = reader.readLine();
+        StringTokenizer tokenizer = new StringTokenizer(line,"\n\r\f");
+        if (tokenizer.hasMoreTokens()) {
+          return tokenizer.nextToken();
+        }
+      } catch (IOException ioe) {
+        System.exit(0);
+      }
+    } while (true);
 } 
   
 //Prompts the user with yes or no and returns bool
 private boolean yesOrNo(String prompt){
-  
+   String more = getToken(prompt + " (Y|y)[es] or anything else for no");
+    if (more.charAt(0) != 'y' && more.charAt(0) != 'Y') {
+      return false;
+    }
+    return true;
 }
 
 //Stores user type (clerk/manager/client)
